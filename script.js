80cade99512797c291c8f55a9c332e5d15e035af
debutcmc <officialdebutcmc@gmail.com>
2025-12-20 13:23:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Mengambil elemen tombol dan teks
+const tombol = document.getElementById('btn-cek');
+const teksStatus = document.getElementById('status-text');
 
+// Fungsi saat tombol diklik
+tombol.addEventListener('click', () => {
+    teksStatus.innerText = "Website ini sedang dipantau oleh CEO DebutCMC!";
+    teksStatus.style.color = "#fbbf24";
+    alert("Semangat Membangun DebutCMC!");
+});
