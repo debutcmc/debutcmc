325b27519595718dcb40ba0cefcda48daf44d3e3
debutcmc <officialdebutcmc@gmail.com>
2025-12-24 12:34:45 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,23 @@
+// js/auth.js
+import { auth } from "./firebase.js";
+import { onAuthStateChanged, signOut } from 
+  "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+export function guardPage(redirect = "index.html") {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = redirect;
+    }
+  });
+}
+
+export function watchAuth(callback) {
+  onAuthStateChanged(auth, callback);
+}
+
+export async function logout() {
+  if (confirm("Logout dari akun?")) {
+    await signOut(auth);
+    location.href = "index.html";
+  }
+}
