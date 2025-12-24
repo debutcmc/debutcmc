78832715814043b4b7ec959ae065b53d1f5dbede
debutcmc <officialdebutcmc@gmail.com>
2025-12-24 12:33:46 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,16 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCt2j9hATOmWYqdknCi05j8zIO59SaF578",
+  authDomain: "debutcmc-ec2ad.firebaseapp.com",
+  projectId: "debutcmc-ec2ad",
+  storageBucket: "debutcmc-ec2ad.firebasestorage.app",
+  appId: "1:283108871954:web:5900298201e74ce83d2dcb"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
